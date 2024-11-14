9f558c9e88899b8b0043ea30759a38b4a6454df5
AP7Works <156809673+AP7Works@users.noreply.github.com>
2024-11-14 15:01:53 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ mongoose.connect('mongodb+srv://annaparaczky:AWYSjKrCIJJ4pMpP@cluster0.22msy.mon
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Connection error", error));
 
+// Basic route for the root URL
+ app.get('/', (req, res) => { 
+res.send('Welcome to the Music App API'); 
+});
+
 // Use the routes
 app.use('/', songRoutes);
 
